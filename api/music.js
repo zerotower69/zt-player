5ff856da86e6c3ae9af0f1650b6f3bafcdf50bba
zerotower69 <zerotower@163.com>
2021-12-28 16:16:16 +0800

    --- Auto-Git Commit ---

--- api/music.js
+++ api/music.js
@@ -12,10 +12,10 @@ export const getMusicInfo = (id) => {
 export const getMusicUrl = (id) => {
     return axios.get(`/song/url?id=${id}`);
 }
-//获取热门歌曲
-export const getHotMusic = (id) => {
-    return axios.get(`/top/list?idx=${id}`);
-}
+// //获取热门歌曲   此接口废弃处理
+// export const getHotMusic = (id) => {
+//     return axios.get(`/top/list?idx=${id}`);
+// }
 //获取搜索建议
 export const getSearchSuggest = (key) => {
     return axios.get(`/search/suggest?keywords=${key}`)
@@ -29,4 +29,12 @@ export const getTopListArtist = (type) => {
 //新歌速递
 export const getNewSongs = (type) => {
     return axios.get(`/top/song?type=${type}`)
+}
+
+//获取音乐详情,ids可以是单个id,也可以是，分割的id,也可以是id数组
+export const getSongDetail = (ids) => {
+    if (Array.isArray(ids)) {
+        ids = ids.join(',')
+    }
+    return axios.get(`/song/detail?ids=${ids}`)
 }
